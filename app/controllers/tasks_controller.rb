c48f67ecfdcd7eec607a63020d91549136aa7b1c
starmie <minminsu0033@gmail.com>
2018-10-16 13:39:46 +0000

    問題点修正01

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     
     def index
         if logged_in?
@@ -69,5 +72,11 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content,:status)
     end
-
+    
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_path
+    end
+  end
 end
